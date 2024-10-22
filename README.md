2a18a7f2295135d8ae7a75f1f732ed63748ba39a
chaeyeon <chaenjung.99@gmail.com>
2024-10-22 15:28:20 +0900

    feat) splash 페이지 개발

--- README.md
+++
@@ -1,2 +0,0 @@
-# e-commerce-app
-flutter, bloc, jira를 통해  e-commerce 앱 구현해보기
--- lib/presentation/pages/splash/splash_page.dart
+++ lib/presentation/pages/splash/splash_page.dart
@@ -1,17 +1,32 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../routes/routes_name.dart';
+
+//TODO 로그인 bloc
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () => context.goNamed(RoutesName.HOME));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('splash'),
-      ),
-      body: Center(
-        child: Text('splashPage'),
-      ),
+      //TODO 하드코딩 된 값 변경할 것
+      body: Center(child: SvgPicture.asset('assets/svg/main_logo.svg')),
+      backgroundColor: Color(0xFF5F0080),
     );
   }
 }
