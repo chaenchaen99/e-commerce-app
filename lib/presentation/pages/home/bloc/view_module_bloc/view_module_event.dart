fe18596d911a4c8b171f2114ceac747a2bdbccff
chaeyeon <chaenjung.99@gmail.com>
2024-10-24 13:43:08 +0900

    feat) ViewModule 리스트 - Bloc 개발

---
+++ lib/presentation/pages/home/bloc/view_module_bloc/view_module_event.dart
@@ -0,0 +1,10 @@
+part of 'view_module_bloc.dart';
+
+abstract class ViewModuleEvent {
+  const ViewModuleEvent();
+}
+
+class ViewModuleInitialized implements ViewModuleEvent {
+  final int tabId;
+  const ViewModuleInitialized(this.tabId);
+}
--- lib/presentation/pages/home/component/global_navigation/global_navigation_bar_view.dart
+++ lib/presentation/pages/home/component/global_navigation/global_navigation_bar_view.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../../core/utils/constant.dart';
 import '../../../../../domain/model/display/menu/menu.model.dart';
+import '../../../../../domain/usecase/display/display.usecase.dart';
+import '../../../../../service_locator.dart';
 import '../../../main/cubit/mall_type_cubit.dart';
+import '../../bloc/view_module_bloc/view_module_bloc.dart';
 
 class GlobalNavigationBarView extends StatelessWidget {
   const GlobalNavigationBarView(this.mallType, this.menus, {super.key});
@@ -12,16 +17,59 @@ class GlobalNavigationBarView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: TabBarView(
-        children: List.generate(
-          menus.length,
-          (index) => Column(
-            children: [
-              Text('mallType: ${mallType}'),
-              Text('mallType: ${menus[index].title}'),
-            ],
-          ),
-        ),
+        children: List.generate(menus.length, (index) {
+          return BlocProvider(
+            create: (_) => ViewModuleBloc(locator<DisplayUsecase>())
+              ..add(ViewModuleInitialized(menus[index].tabId)),
+            child: ViewModuleList(),
+          );
+        }),
       ),
     );
+  }
+}
+
+class ViewModuleList extends StatelessWidget {
+  const ViewModuleList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ViewModuleBloc, ViewModuleState>(builder: (_, state) {
+      switch (state.status) {
+        case Status.initial:
+        case Status.loading:
+          return Center(child: CircularProgressIndicator());
+        case Status.success:
+          return Column(
+            children: [
+              Container(
+                color: Colors.deepOrangeAccent,
+                height: 50,
+                child: Center(
+                  child: Text('${state.tabId}'),
+                ),
+              ),
+              Expanded(
+                child: ListView.separated(
+                  itemBuilder: (_, index) {
+                    return SizedBox(
+                      height: 200,
+                      child: Center(
+                        child: Text('${state.viewModules[index].type}'),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (_, index) => Divider(thickness: 4),
+                  itemCount: state.viewModules.length,
+                ),
+              ),
+            ],
+          );
+        case Status.error:
+          return Center(
+            child: Text('error'),
+          );
+      }
+    });
   }
 }
